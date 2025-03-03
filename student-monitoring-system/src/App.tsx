846b0a464b9081dbccf13fd49767c75ccd03cf9d
romnick <romnicksanjuan9@gmail.com>
2025-03-03 21:11:59 +0800

    nice

--- student-monitoring-system/src/App.tsx
+++ student-monitoring-system/src/App.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter,Routes,Route } from "react-router-dom"
 import Create from "./components/Create"
 import Login from "./components/Login"
 import AdminDashboard from "./components/admin-components/AdminDashboard"
-import DriverDashboard from "./components/bus-driver components/DriverDashboard"
 import BusManifest from "./components/admin-components/BusManifest"
 import Attendance from "./components/admin-components/Attendance"
 import Students from "./components/admin-components/Students"
@@ -23,7 +22,6 @@ function App() {
         <Route path="create-user" element={< Create />}/>
         <Route path="/" element={< Login />}/>
         <Route path="admin/dashboard" element={< AdminDashboard />}/>
-        <Route path="bus-driver/dashboard" element={< DriverDashboard />}/>
         <Route path="bus-manifest" element={<BusManifest />}/>
         <Route path="attendance" element={<Attendance />}/>
         <Route path="students" element={<Students />}/>
