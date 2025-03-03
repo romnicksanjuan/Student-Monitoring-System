cadde67d412e3b53b80187161736254de8174cb0
romnick <romnicksanjuan9@gmail.com>
2025-03-03 22:14:44 +0800

    nicee

--- student-monitoring-system/src/components/admin-components/StudentForm.tsx
+++ student-monitoring-system/src/components/admin-components/StudentForm.tsx
@@ -52,7 +52,7 @@ const StudentForm = () => {
     };
 
     return (
-        <div className="flex justify-center items-center h-screen">
+        <div className="flex justify-center h-screen">
             <div className="max-w-xl mx-auto p-6 bg-white shadow-lg rounded-lg">
                 <h2 className="text-2xl font-bold mb-4">Student Registration Form</h2>
                 {message ? <h2 className="text-lg text-center bg-green-500 py-2 my-2.5 rounded-md">{message}</h2> :''}
